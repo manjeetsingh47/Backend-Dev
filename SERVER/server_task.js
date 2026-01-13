d95169f4145c36cb26c09e7187f3570ce4b3ce1f
manjeetsingh47 <7347nmanjeet@gmail.com>
2026-01-13 15:09:36 +0530

    [Manjeet] added task to create server

--- SERVER/server_task.js
+++ SERVER/server_task.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
         res.end(" Welcome ");
     } else if (req.url === "/about") {
         res.writeHead(200, { "content-type": "text/html" });
-        res.end("<h1> About Page </h1>")
+        res.end(" About Page ")
     } else if (req.url === "/user") {
         const user = {
             name: "deepak",
@@ -21,7 +21,7 @@ const server = http.createServer((req, res) => {
         }));
     } else {
         res.writeHead(200, { "content-type": "text/html" });
-        res.end("<h1> Page not found </h1>")
+        res.end(" Page not found ")
     }
 
 });
