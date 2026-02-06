5864e79a4e9e834cb304b33f7b4c3a2ee7b9b198
manjeetsingh47 <7347nmanjeet@gmail.com>
2026-02-06 14:54:16 +0530

    [edit EJS]

--- EJS/server.js
+++ EJS/server.js
@@ -1,69 +1,82 @@
-import express from 'express';
+import express from "express";
+import methodOverride from "method-override"
+
 const app = express();
 
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true })); // to parse form data
-//static server
-//csr = client side rendering
-//ssr = server side rendering - seo friendly (search engine optimization) fast than csr
-//template engine
-//ejs , pug , hbs
-//react = csr 
-//ejs - template engine runs dynamic html pages on server side (with help of express js)
+app.set("view engine", "ejs");
+app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.use(express.urlencoded({ extended: true }))
 
-// app.get('/user', (req, res) => {
-//     //binding data to ejs template
-//     let userData = { 
-//         name: 'John Doe',
-//         age: 30,
-//     };
-//     res.render('user',{userData});
-// });
+
 let userData = [
-    { id: 1, name: 'John Doe', age: 30 },
-    { id: 2, name: 'Jane Smith', age: 25 },
-    { id: 3, name: 'Mike Johnson', age: 35 },
+    { id: 1, name: "amit", age: 23, },
 ];
 
-app.get('/user', (req, res) => {
-    res.render('user', { userData });
+// render index page
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
-app.post('/api/user', (req, res) => {
+//render edit page
+app.get("/editpage/:id", (req, res) => {
+    const id = req.params.id;
+
+    const user = userData.find((ele) => ele.id == id);
+    console.log(user)
+
+    res.render("edit", { userData: [user] })
+})
+
+// get user
+app.get("/user", (req, res) => {
+    res.render("user", { userData });
+});
+
+// add user
+app.post("/api/user", (req, res) => {
+
     const { name, age } = req.body;
+
     let newUserData = {
         id: userData.length + 1,
         name,
-        age,
+        age
     }
     userData.push(newUserData);
-    res.redirect('/user');
-});
+    res.redirect('/user')
 
-app.get("/api/user/:id", (req, res) => {
-    const userId = parseInt(req.params.id);
-    const useridx = userData.findIndex(u => u.id === userId);
-    if (useridx === -1) {
-        return res.status(404).render('404');
+})
+
+//delete user
+app.delete("/api/user/:id", (req, res) => {
+
+    const userid = req.params.id;
+
+    const useridx = userData.findIndex((ele) => ele.id == userid);
+
+    if (useridx == -1) {
+        return res.send("user not found")
     }
 
     userData.splice(useridx, 1);
-    res.redirect('/user');
-});
 
-app.get('/list', (req, res) => {
-    let arr = ["apple", "banana", "grapes", "mango"];
-    res.render('list', { arr });
-});
+    res.redirect("/user")
 
-app.use((req, res) => {
-    res.status(404).render('404');
-});
+})
+
+app.put("/api/user/:id", (req, res) => {
+    const { name, age } = req.body;
+})
 
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+    console.log("server is running");
 });
+
+
+// staic server
+//csr
+//ssr
+// template engine
+// ejs, pug, hbs
+//seo friendly
