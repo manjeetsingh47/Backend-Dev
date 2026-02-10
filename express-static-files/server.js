41d2b8e516cf4780d6a1ffd7b647c4a19724f73b
manjeetsingh47 <7347nmanjeet@gmail.com>
2026-02-10 15:01:41 +0530

    [Manjeet] add images using ejs

--- express-static-files/server.js
+++ express-static-files/server.js
@@ -2,12 +2,24 @@ import express from "express";
 
 const port = 3000;
 const app = express();
-app.use("/static", express.static('public'));
+
+// static files
+app.use("/static", express.static("public"));
+
+// ejs setup
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.get("/", (req, res) => {
-    res.send("home");
-})
+
+    const images = [
+        "/static/img1.jpg",
+        "/static/img2.jpg"
+    ];
+
+    res.render("gallery", { images });
+});
 
 app.listen(port, () => {
     console.log("The server is running " + port);
-})
+});
